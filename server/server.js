34ed1cd25bb4e92f81ce9329a9c32a29cb808967
Dark1nessss <frankhorn99@gmail.com>
2024-09-15 11:54:18 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,10 +16,9 @@ app.use('/api', userRoutes);
 app.use('/api', accountRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
