9a2c4def9917188d0128f11496661602e944068f
Dark1nessss <frankhorn99@gmail.com>
2024-09-25 18:10:08 +0100

    Update Product Section
    V0.2

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className="bg-[#141313] text-white">
       {/* Welcome Section */}
@@ -43,47 +45,46 @@ const LandingPage = () => {
       </section>
 
       {/* Products Section */}
-      <section className="products-section bg-[#141313] text-white lg:ml-16 mb-16">
+      <section className="products-section bg-[#141313] text-white lg:ml-16 pb-8">
         <div className="lg:px-56 px-6">
-          <h2 className="text-4xl font-bold mb-6">
-            Our <span className="text-[#A5E404]">Products</span>
-          </h2>
-          <p className="text-lg mb-12">
-            Discover a range of comprehensive and customizable banking products at YourBanK, designed to suit your unique financial needs and aspirations.
-          </p>
-
-          {/* Toggle Buttons */}
-          <div className="flex justify-start space-x-4 mt-4 lg:ml-1 mb-8 lg:mb-16">
-            <button className="bg-[#A5E404] text-black py-2 px-6 rounded-full font-semibold">
-              For Individuals
-            </button>
-            <button className="bg-[#2B2B2B] text-white py-2 px-6 rounded-full font-semibold">
-              For Businesses
-            </button>
-          </div>
-
-          {/* Product Cards */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:px-4 mt-8">
-            <div className="product-card bg-[#1C1C1C] p-8 rounded-lg flex flex-col items-center text-center">
-              <img src="/checkings.png" alt="Checking Accounts" className="w-16 h-16 mb-6" />
-              <h3 className="text-xl font-semibold mb-4">Checking Accounts</h3>
-              <p className="text-sm">
-                Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access.
+          <div className="flex justify-between items-center">
+            <div>
+              <h2 className="text-4xl font-bold mb-6">
+                Our <span className="text-[#A5E404]">Products</span>
+              </h2>
+              <p className="text-lg mb-12 max-w-4xl">
+                Discover a range of comprehensive and customizable banking products at YourBanK, designed to suit your unique financial needs and aspirations.
               </p>
             </div>
-            <div className="product-card bg-[#1C1C1C] p-8 rounded-lg flex flex-col items-center text-center">
-              <img src="/savings.png" alt="Savings Accounts" className="w-16 h-16 mb-6" />
-              <h3 className="text-xl font-semibold mb-4">Savings Accounts</h3>
-              <p className="text-sm">
-                Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you.
-              </p>
-            </div>
-            <div className="product-card bg-[#1C1C1C] p-8 rounded-lg flex flex-col items-center text-center">
-              <img src="/loans.png" alt="Loans and Mortgages" className="w-16 h-16 mb-6" />
-              <h3 className="text-xl font-semibold mb-4">Loans and Mortgages</h3>
-              <p className="text-sm">
-                Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need.
-              </p>
+
+            {/* Toggle Buttons */}
+            <div className="relative flex justify-center items-center w-64 h-14 bg-[#2B2B2B] rounded-full overflow-hidden">
+              {/* Background Highlight */}
+              <div
+                className={`absolute h-2/3 w-[45%] bg-[#A5E404] rounded-full transition-transform duration-300 ease-in-out ${
+                  isActive ? "translate-x-[100%]" : "translate-x-0"
+                }`}
+              ></div>
+
+              {/* Button for Individuals */}
+              <button
+                className={`z-10 w-1/2 text-md font-medium ${
+                  !isActive ? "text-black" : "text-white"
+                } transition-colors duration-300`}
+                onClick={() => setIsActive(false)}
+              >
+                For Individuals
+              </button>
+
+              {/* Button for Businesses */}
+              <button
+                className={`z-10 w-1/2 text-md font-medium ${
+                  isActive ? "text-black" : "text-white"
+                } transition-colors duration-300`}
+                onClick={() => setIsActive(true)}
+              >
+                For Businesses
+              </button>
             </div>
           </div>
         </div>
