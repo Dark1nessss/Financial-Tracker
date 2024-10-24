78cc3b1e615452dc66f5184a31b1098c5d694c11
Dark1nessss <frankhorn99@gmail.com>
2024-10-24 10:14:27 +0100

    Security

---
+++ client/src/pages/Security.js
@@ -0,0 +1,13 @@
+import Welcome from '../components/sections/security/Welcome.js';
+
+const Security = () => {
+
+  return (
+    <div className="bg-[#141313] text-white">
+      <Welcome/>
+      
+    </div>
+  );
+};
+
+export default Security;
