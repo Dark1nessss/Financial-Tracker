c34cf2aef9c1b20dc4ef2845f612b5483646f486
Dark1nessss <frankhorn99@gmail.com>
2024-10-25 11:18:54 +0100

    Faq Security
    V0.1

--- client/src/pages/Security.js
+++ client/src/pages/Security.js
@@ -1,13 +1,16 @@
 import Welcome from '../components/sections/security/Welcome.js';
 import Protect from '../components/sections/security/Protect.js';
+import Faq from '../components/sections/security/Faq.js';
 
 const Security = () => {
 
   return (
     <div className="bg-[#141313] text-white">
       <Welcome/>
-      
+
       <Protect/>
+
+      <Faq/>
     </div>
   );
 };
