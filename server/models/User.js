859217d8fbbbef52ad81aeb908abf9aa8c5c96ca
Tanjil <tanjilkh@gmail.com>
2024-11-03 11:32:11 +0000

    feat: implement passwordConfirm validator and add hash pre method

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,49 @@
-const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   username: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true 
+    lowercase: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords do not match",
+    },
+    select: false,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre("save", async function (next) {
+  // only run this function if password was actually changed
+  if (!this.isModified("password")) return next();
+
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field to avoid saving it in the database
+  this.passwordConfirm = undefined;
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
