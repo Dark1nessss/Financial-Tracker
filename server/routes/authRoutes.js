9d3acb3ed7c2b43b14d7437dd1abd85efae4e3dd
Tanjil <tanjilkh@gmail.com>
2024-11-03 16:36:40 +0000

    feat: Add verify reset token

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,5 +6,7 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
+router.patch("/resetPassword/", authController.resetPassword);
+router.post("/verifyResetToken", authController.verifyResetToken);
 
 module.exports = router;
