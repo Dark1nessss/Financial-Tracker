3d24d3e7472162535741704752a8f81cea9c1b15
Tanjil <tanjilkh@gmail.com>
2024-11-04 12:03:49 +0000

    feat: Implement protected route into dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Support from './pages/Support';
-import Careers from './pages/Careers';
-import About from './pages/About';
-import Security from './pages/Security';
-import Dashboard from './pages/Dashboard';
-import ForgotPassword from './pages/ForgotPassword';
-import NewPassword from './pages/NewPassword'; 
-import CodeCheck from './pages/CodeCheck';
-import Layout from './components/layout/Layout';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Support from "./pages/Support";
+import Careers from "./pages/Careers";
+import About from "./pages/About";
+import Security from "./pages/Security";
+import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/ForgotPassword";
+import NewPassword from "./pages/NewPassword";
+import CodeCheck from "./pages/CodeCheck";
+import Layout from "./components/layout/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -22,13 +23,15 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/support" element={<Support />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/security" element={<Security />} />
           <Route path="/about" element={<About />} />
           <Route path="/forgotpass" element={<ForgotPassword />} />
           <Route path="/newpassword" element={<NewPassword />} />
           <Route path="/code" element={<CodeCheck />} />
+          <Route path="/dashboard" element={<ProtectedRoute />}>
+            <Route index element={<Dashboard />} />
+          </Route>
         </Routes>
       </Layout>
     </Router>
